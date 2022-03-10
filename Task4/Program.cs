5e8f5772f5e5fe43d6e1d823bbd840b9606147a7
Ваше имя <garaevaea@mail.ru>
2022-03-10 07:39:04 +0300

    Add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int numberA = new int();
+int numberB = new int();
+int numberC = new int();
+
+Console.Write("Введите первое число: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+numberC = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+
+if (numberB > max) max = numberB;
+
+if (numberC > max) max = numberC;
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
