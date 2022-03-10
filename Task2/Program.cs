2ae976d789636deb67f5262a435e1cc660a173a1
Ваше имя <garaevaea@mail.ru>
2022-03-10 07:30:25 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿int numberA = new int();
+int numberB = new int();
+
+Console.Write("Введите первое число: ");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+numberB = Convert.ToInt32(Console.ReadLine());
+
+int max = numberA;
+int min = numberB;
+
+if (numberB > max) 
+{
+    max = numberB;
+    min = numberA;
+    Console.Write("Большее число = ");
+    Console.WriteLine(max);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(min);
+}
+
+else
+{
+    Console.Write("Большее число = ");
+    Console.WriteLine(max);
+    Console.Write("Меньшее число = ");
+    Console.WriteLine(min);
+}
