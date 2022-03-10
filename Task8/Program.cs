25445962880d32ab49acbfb8a0c2202c00004a2c
Ваше имя <garaevaea@mail.ru>
2022-03-10 08:27:38 +0300

    Add Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿int N = new int();
+
+Console.Write("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+while(count <= N)
+{
+    if(count % 2 == 0)
+    {
+       Console.WriteLine(count); 
+    }
+    count++;
+}
